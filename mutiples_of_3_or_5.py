75e1f2c510543a894a46d83c230af882e12e9dfe
Terrain <155668627+Terushion@users.noreply.github.com>
2024-08-20 19:43:11 +0100

    Update mutiples_of_3_or_5.py

--- mutiples_of_3_or_5.py
+++ mutiples_of_3_or_5.py
@@ -16,27 +16,33 @@ output = 78
 
 """
 
-def solution():
+def solution(num):
 
-    num = int(input("Enter a number to find the sum of all multiples of 3 & 5 below it:\n"))
+    # num = int(input("Enter a number to find the sum of all multiples of 3 & 5 below it:\n"))
 
     # if num != int():
     #     print("Error! That isn't correct! Please enter an integer next time.")
 
     sum_of = 0
 
-    if num < 0:
-        return 0
-    else:
-        for x in range(0, num - 1):
-            if x % 3 or 5 == 0:
-                sum_of += x
-            else:
-                pass
-    
+    for x in range(1, num):
+        # This will iterate over every number from one 
+        # to the integer that was an input in the function
+        if x % 3 == 0:
+            sum_of += x
+        elif x % 5 == 0:
+            sum_of += x   
+        # If an integer that is divided by 3 or 5 does not have a remainder
+        # Then it is a multiple, so it will be added to the sum
+        # If 'x' is a mulutiple of 3 and 5, it will only be added to the sum once
+        # The reason being is because if it meets the first condition,
+        # if x % 3 == 0, it will be added to the sum automatically.
+        else:
+            pass
+
     return sum_of
 
 
-print(solution())
+print(solution(16))
 
 
