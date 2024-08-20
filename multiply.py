e646de1bbef4fee785b336b03670a79ceaafc4e5
Terrain <155668627+Terushion@users.noreply.github.com>
2024-08-20 20:17:31 +0100

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,18 @@
+# multiply.py
+
+"""
+This code does not execute properly. Try to figure out why.
+
+def multiply(a, b):
+    a * b
+
+"""
+
+def multiply(a, b):
+    return a * b
+
+print(multiply(2, 5))   # Expected outcome: 10
+print(multiply(8, 12))   # Expected outcome: 96
+print(multiply(3, 7))   # Expected outcome: 21
+print(multiply(10, 0))   # Expected outcome: 0
+print(multiply(-10, -1))   # Expected outcome: 10
